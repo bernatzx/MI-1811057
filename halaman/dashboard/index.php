368f8980f82949ada1f754d0fea91439b3074bec
bernat <andikasukardi1@gmail.com>
2025-07-25 18:32:34 +0900

    init dashboard

--- halaman/dashboard/index.php
+++ halaman/dashboard/index.php
@@ -1,3 +1,14 @@
 <?php include_once '../../header.php' ?>
-<h2>Dashboard</h2>
+<div class="col-8 m-auto p-4" style="background-color: #fff; height: 90vh;">
+    <h4>Selamat Datang</h4>
+    <div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
+        <div>
+            <h1 class="text-center">
+                Sistem Pendukung Keputusan Kepuasan Pelanggan Di Minimarket Indomaret Kota Ternate dengan Metode
+                Weighted
+                Product (WP)
+            </h1>
+        </div>
+    </div>
+</div>
 <?php include_once '../../footer.php' ?>
