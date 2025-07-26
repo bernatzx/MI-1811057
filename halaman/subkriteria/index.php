6485549ff63e5889f685adb0383098af8d944660
bernat <andikasukardi1@gmail.com>
2025-07-26 13:25:00 +0900

    modif1 index subkriteria

--- halaman/subkriteria/index.php
+++ halaman/subkriteria/index.php
@@ -1,3 +1,56 @@
 <?php include_once '../../header.php' ?>
-<h2>Subkriteria</h2>
+<div class="col-8 m-auto p-4" style="background-color: #fff;">
+    <div class="d-flex align-items-center justify-content-between mb-3">
+        <div>
+            <h4>Subkriteria</h4>
+        </div>
+        <div class="d-flex align-items-center gap">
+            <div>
+                <a href="" class="btn btn-sm btn-outline-secondary"><i class="fas fa-refresh"></i></a>
+            </div>
+            <div>
+                <a href="tambah.php" class="d-flex align-items-center gap btn btn-sm btn-success"><i
+                        class="fas fa-plus"></i>Tambah Subkriteria</a>
+            </div>
+        </div>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Kriteria</th>
+                    <th>Subkriteria</th>
+                    <th>Nilai</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $i = 0;
+                $sql = mysqli_query($hub, 'SELECT s.*, k.nama_kriteria FROM tb_subkriteria s JOIN tb_kriteria k ON s.id_kriteria = k.id_kriteria') or die(mysqli_error($hub));
+                if (mysqli_num_rows($sql) > 0) {
+                    while ($row = mysqli_fetch_assoc($sql)) {
+                        $i++; ?>
+                        <tr>
+                            <td><?= $i ?></td>
+                            <td><?= $row['nama_kriteria'] ?></td>
+                            <td><?= $row['nama_subkriteria'] ?></td>
+                            <td><?= $row['nilai_subkriteria'] ?></td>
+                            <td class="d-flex gap">
+                                <a href="ubah.php?id=<?= $row['id_subkriteria'] ?>"
+                                    class="gap d-flex align-items-center btn btn-outline-info btn-sm"><i
+                                        class="fas fa-pencil"></i>Edit</a>
+                                <a onclick="return confirm('Anda akan menghapus subkriteria <?= $row['nama_subkriteria'] ?>?')"
+                                    href="hapus.php?id=<?= $row['id_subkriteria'] ?>" class="btn btn-sm btn-danger"><i
+                                        class="fas fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    <?php }
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 <?php include_once '../../footer.php' ?>
